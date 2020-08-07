c340ac7c45311bb708130c123ef8883c210285df
Ricardo Bustos <ricardobustos14@outlook.com>
2020-08-06 21:18:41 -0300

    Primera version

---
+++ script.js
@@ -0,0 +1,49 @@
+function playWorkIt1(){
+    /* document.getElementById("workit1").load(); */
+    document.getElementById("workit1").play();
+}
+function playMakeIt1(){
+    document.getElementById("makeit1").play();
+}
+function playDoIt1(){
+    document.getElementById("doit1").play();
+}
+function playMakeUs1() {
+    document.getElementById("makeus1").play();
+}
+function playHarder1() {
+    document.getElementById("harder1").play();
+}
+function playBetter1() {
+    document.getElementById("better1").play();
+}
+function playFaster1() {
+    document.getElementById("faster1").play();
+}
+function playStronger1() {
+    document.getElementById("stronger1").play();
+}
+function playMoreThan1() {
+    document.getElementById("morethan1").play();
+}
+function playHour1() {
+    document.getElementById("hour1").play();
+}
+function playOur1() {
+    document.getElementById("our1").play();
+}
+function playNever1() {
+    document.getElementById("never1").play();
+}
+function playEver1() {
+    document.getElementById("ever1").play();
+}
+function playAfter1() {
+    document.getElementById("after1").play();
+}
+function playWorkIs1() {
+    document.getElementById("workis1").play();
+}
+function playOver1() {
+    document.getElementById("over1").play();
+}
